06447a9ac627a67272b1ed56d9ea953d70c1bf74
Vitaliy Liubezny <Vitaliy_Liubezny@epam.com>
2016-06-03 11:06:46 +0300

    Fix packaging issue -> 1.0.4

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require("gulp");
 var zip = require("gulp-zip");
 
 gulp.task("default", function() {
-  return gulp.src("src/*")
+  return gulp.src("src/**")
     .pipe(zip("YandexPlayer.zip"))
     .pipe(gulp.dest("dist"));
 });
